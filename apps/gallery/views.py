cde0f80bb5834da65544eba58bb0f3fab7aa7a5a
Leonardo Amaral <leoamaral.oliv@gmail.com>
2024-08-05 20:04:40 -0300

    Release/issue/15 - Organizing and refactoring

--- apps/gallery/views.py
+++ apps/gallery/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
-from gallery.models import Photography
+from apps.gallery.models import Photography
 
 
 def index(request):
