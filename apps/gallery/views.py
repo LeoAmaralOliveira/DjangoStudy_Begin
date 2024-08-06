6f1a4cbe5c04595edeab9bd2e2581c054ee96e42
Leonardo Amaral <leoamaral.oliv@gmail.com>
2024-08-05 22:00:30 -0300

    Release/issue/15 - Images CRUD

--- apps/gallery/views.py
+++ apps/gallery/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from apps.gallery.models import Photography
+from apps.gallery.forms import PhotographyForms
 
 
 def index(request):
@@ -34,3 +35,40 @@ def search(request):
             photographies = photographies.filter(name__icontains=name_to_search)
 
     return render(request, 'gallery/search.html', {"cards": photographies})
+
+
+def new_image(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+
+    form = PhotographyForms
+    if request.method == 'POST':
+        form = PhotographyForms(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Nova fotografia cadastrada!')
+            return redirect('index')
+
+    return render(request, 'gallery/new_image.html', {'form': form})
+
+
+def edit_image(request, photo_id):
+    photography = Photography.objects.get(id=photo_id)
+    form = PhotographyForms(instance=photography)
+
+    if request.method == 'POST':
+        form = PhotographyForms(request.POST, request.FILES, instance=photography)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Fotografia editada com sucesso!')
+            return redirect('index')
+
+    return render(request, 'gallery/edit_image.html', {'form': form, 'photo_id': photo_id})
+
+
+def delete_image(request, photo_id):
+    photography = Photography.objects.get(id=photo_id)
+    photography.delete()
+    messages.success(request, 'Fotografia deletada com sucesso!')
+    return redirect('index')
